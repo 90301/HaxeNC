3449e8151a8ea6141ac9e510df4c3b92760c6744
NighttimeDriver50000 <chrismckinney@me.com>
2014-10-26 08:49:58 -0400

    Hud super fix

--- HelloWorld/source/HUD.hx
+++ HelloWorld/source/HUD.hx
@@ -3,6 +3,7 @@ import flixel.ui.FlxButton;
 import flixel.FlxSprite;
 class HUD extends FlxSprite {
     public function new() {
+        super();
         var i = 0;
         for (goodi in Main.player.inventory) {
             var good = goodi.goodRef;
