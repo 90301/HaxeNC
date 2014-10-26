32688105a742a12d5da91c2a37ac8da83963c209
90301 <tbsw_productions@hotmail.com>
2014-10-26 02:50:19 -0400

    Started master good list

--- HelloWorld/source/GoodMaster/MasterGoodList.hx
+++ HelloWorld/source/GoodMaster/MasterGoodList.hx
@@ -3,6 +3,12 @@ class MasterGoodList {
     public var goodList:Array<Good> = new Array<Good>();
     public function new() {
     //CREATE ALL THE GOODS!!!!!
+    //food (name , type , mass , sation , deterioration)
+    var tempGood:Good = new Good("Cookies","food",1,1,5);
+    goodList.push(tempGood);
+
+    var tempGood:Good = new Good("Brownies","food",2,1,5);
+    goodList.push(tempGood);
 
     }
 }
