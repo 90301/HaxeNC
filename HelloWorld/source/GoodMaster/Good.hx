e88686ab6107534a42ab13dfd56cf9fff8899d27
90301 <tbsw_productions@hotmail.com>
2014-10-26 03:22:47 -0400

    Added support for weapons

--- HelloWorld/source/GoodMaster/Good.hx
+++ HelloWorld/source/GoodMaster/Good.hx
@@ -16,6 +16,25 @@ class Good {
         type = goodLookup(typeL);
         if (type==0) {
             createFood(v1,v2);
+        } else {
+            trace("You dun goofed and typed food wrong");
+        }
+    }
+    public function new(nameL:String,typeL:String,massL:Int,v1:Int,v2:Int,v3:Bool,v4:Int) {
+    //four args
+    //weapon
+    //v1:damage
+    //v2:speed (wait time to attack again, 60 cycles per sec so speed=60 is 1 atk/sec 30= 2 atk/sec)
+    //v3:ranged weapon? (true if ranged, false if melee)
+    //v4:range (1 distance unit per point)
+
+
+        name = nameL;
+        type = goodLookup(typeL);
+        if (type==1) {
+            createWeapon(v1,v2,v3,v4);
+        } else {
+            trace("You dun goofed and typed weapon wrong");
         }
     }
 
@@ -24,6 +43,10 @@ class Good {
     }
     public function createWeapon(damage:Int,speed:Int,projectile:Bool,range:Int):Void {
     //4 args
+        if (projectile!=true && range!=1) {
+            range = 1;
+        }
+
     }
     public function goodLookup(str:String):Int {
         var rtrn = -1;
